b0d8382b9ed15f72df93b69fdaf44fbbb38550b4
Daniel Uzunu <daniel.uzunu@gmail.com>
2012-06-04 13:06:23 +0300

    some changes

--- server/app.js
+++ server/app.js
@@ -1,6 +1,15 @@
 var express = require('express'),
 	path = require('path'),
 	app = express.createServer();
+
+app.use(express.bodyParser());
+app.use(app.router);
 app.use(express.static(path.join(__dirname, '..', 'web')));
+
+app.post('/upload', function (req, res) {
+	console.log(req.body);
+	res.end('done');
+});
+
 app.listen(3000);
 console.log('server started');
