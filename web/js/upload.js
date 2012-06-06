89100630346a75aaeefa95073b531a6d497b3655
Daniel Uzunu <daniel.uzunu@gmail.com>
2012-06-06 23:09:53 +0300

    some small changes

--- web/js/upload.js
+++ web/js/upload.js
@@ -1,19 +1,37 @@
 ﻿var UploadJS = {};
 
 (function () {
+    /**
+     * Uploader class
+     * @constructor
+     * @param {String} id the id of the input type="file" element
+     */
     var Uploader = function (id) {
-        this.elem = document.getElementById(id);
-        if (!this.elem) {
+        var self = this;
+
+        this.upload = document.getElementById(id);
+        this.btn = document.getElementById('btnUpload');
+
+        if (!this.upload) {
             throw new Error('The specifed id does not exist in DOM');
         }
 
-        this.elem.addEventListener('change', function () {
-            console.log(this.files);
-        }, false);
+        this.btn.addEventListener('click', function () {
+            self.upload.click();
+        });
+
+        this.upload.addEventListener('change', function () {
+            self.processFiles(self.upload.files);
+        });
     };
 
-    Uploader.prototype.method = function () {
-        
+    /**
+     * Processes the selected files when the user selects new files to upload.
+     * @param files currently selected files
+     */
+    Uploader.prototype.processFiles = function (files) {
+        console.log(files);
+        window.a = files;
     };
 
     UploadJS.Uploader = Uploader;
