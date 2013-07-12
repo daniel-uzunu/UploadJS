8115c91ce6a29b38069ddb5ac77f917452ad34ed
Daniel Uzunu <daniel.uzunu@gmail.com>
2013-07-12 07:38:10 -0400

    cloud9 changes

--- tests/client/server.js
+++ tests/client/server.js
@@ -20,11 +20,13 @@
 
 var express = require('express'),
     path = require('path'),
-    app = express();
+    app = express(),
+    port = process.env.PORT || 3000,
+    ip = process.env.IP;
 
 app.use('/', express.static(path.join(__dirname, 'setup')));
 app.use('/mocha', express.static(path.join(__dirname, 'mocha')));
 app.use('/tests', express.static(path.join(__dirname, 'tests')));
 app.use('/src', express.static(path.join(__dirname, '../../web/js')));
 
-app.listen(5000);
+app.listen(port, ip);
