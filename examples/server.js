3dbd33940d76dc929c963fa18f812270f01a526f
Daniel Uzunu <daniel.uzunu@gmail.com>
2013-08-07 23:43:42 +0300

    very basic example

--- examples/server.js
+++ examples/server.js
@@ -20,12 +20,70 @@
 
 var express = require('express'),
     path = require('path'),
+    crypto = require('crypto'),
+    util = require('util'),
     app = express(),
     port = process.env.PORT || 3000,
-    ip = process.env.IP;
+    ip = process.env.IP,
+    uploads = {};
 
 app.use('/', express.static(path.join(__dirname, 'web')));
 app.use('/src', express.static(path.join(__dirname, '../client')));
+app.use(express.bodyParser());
 
-app.listen(port, ip);
+app.post('/upload', function (req, res) {
+    var id = crypto.randomBytes(24).toString('hex'),
+        url = '/upload/' + id,
+        file = req.body;
+
+    uploads[id] = {
+        name: file.name,
+        size: file.size,
+        buffer: new Buffer(file.size),
+        uploadedBytes: 0
+    };
+
+    res.end(JSON.stringify({url: url}));
+});
+
+// for now I assume that file pieces are sent in order
+app.put('/upload/:id', function (request, response) {
+    var file = uploads[request.params.id],
+        contentLength = parseInt(request.headers['content-length'], 10),
+        regex = /^bytes (\d*)-(\d*)\/(\d*)$/,
+        matches = request.headers['content-range'].match(regex);
+
+    if (!matches || !file) {
+        response.statusCode = 400;
+        response.end();
+        return;
+    }
+
+    var start = parseInt(matches[1], 10);
+
+
+    request.on('data', function (data) {
+        data.copy(file.buffer, start);
+        start += data.length;
+    });
+
+    request.on('end', function () {
+        file.uploadedBytes += contentLength;
+
+        if (file.uploadedBytes !== file.size) {
+            response.writeHead(200, {
+                'Range': util.format('bytes=0-%s', file.uploadedBytes)
+            });
+        } else {
+            response.statusCode = 201;
+            // this buffer should be written to file
+            console.log(file.buffer.toString());
+        }
+
+        response.end();
+    });
+
+});
+
+var server = app.listen(port, ip);
 console.log('server started on: ' + port);
